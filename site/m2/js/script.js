61c3c6d4db05c1f417717ab54cb91d525b3ab138
new53 <new53@users.noreply.github.com>
2017-03-22 11:45:58 +0530

    Create script.js

---
+++ site/m2/js/script.js
@@ -0,0 +1,29 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+
+            function question() {
+ 	var x = document.getElementById("name").value;
+	if(name=="x")	
+	{
+		obj = JSON.parse(text);
+		document.getElementById("display").innerHTML =
+		obj.question[1].first;
+
+    //var d = new Date(x);
+    //var n = d.getDay()
+    //var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+    //document.getElementById("display").innerHTML = days[n];
+            });
+      });
+  
+  }
+);
